1d2ae81c265532ebc0616801fcdd159d776cb24e
victor.wang <fengxuanwang@qq.com>
2018-09-20 15:52:08 +0800

    deliver

--- BigCrabService/src/main/java/com/victor/wang/bigCrab/manager/CardManager.java
+++ BigCrabService/src/main/java/com/victor/wang/bigCrab/manager/CardManager.java
@@ -22,6 +22,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
 import java.util.List;
@@ -206,6 +207,7 @@ public class CardManager
 		return card;
 	}
 
+	@Transactional
 	public Deliver redeem(String cardNumber, @AssertValid CardRedeemRequest redeemRequest)
 	{
 		CardValidateRequest request = new CardValidateRequest();
